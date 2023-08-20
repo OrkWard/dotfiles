813ee87d53b99941568b41db111320c492a2d025
Ubuntu <orkward@main.dostciog0hoevkjvghyskemrqa.bx.internal.cloudapp.net>
2023-08-20 13:37:10 +0000

    setup script, update bashrc

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/sh
+
+WORKDIR="$(realpath "$(dirname "$0")")"
+
+# bash
+rm $HOME/.bashrc
+ln -s $WORKDIR/.bashrc $HOME/.bashrc
+
+# vim
+rm $HOME/.vimrc
+ln -s $WORKDIR/.vimrc $HOME/.vimrc
+
+if [ -d $HOME/.vim/autoload ]; then
+    rm -r $HOME/.vim/autoload
+fi
+
+mkdir -p $HOME/.vim
+ln -s $WORKDIR/autoload $HOME/.vim/autoload
