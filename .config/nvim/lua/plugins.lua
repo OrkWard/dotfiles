f0510568e011f65512dda9ac7d9dec38c9924bfc
OrkWard <orkward_o@outlook.com>
2024-01-04 14:00:06 +0800

    backup: Thursday, January 04, 2024 PM02:00:06 HKT

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -15,15 +15,9 @@ return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
   use 'wakatime/vim-wakatime'
   use 'psliwka/vim-smoothie'
-  use {
-    'smoka7/hop.nvim',
-    tag = '*', -- optional but strongly recommended
-    config = function()
-      -- you can configure Hop the way you like here; see :h hop-config
-      require'hop'.setup { keys = 'etovxqpdygfblzhckisuran' }
-    end
-  }
   use 'tpope/vim-surround'
+  use 'tpope/vim-repeat'
+  use 'ggandor/leap.nvim'
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
