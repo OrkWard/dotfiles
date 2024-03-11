94600928af06dff75eff6713a9cf96e23ce0ac44
caizelun <caizelun.1@bytedance.com>
2024-03-11 13:31:43 +0800

    backup: Mon Mar 11 13:31:43 CST 2024

---
+++ .local/bin/proxy.sh
@@ -0,0 +1,6 @@
+networksetup -setwebproxystate "Wi-Fi" on
+networksetup -setwebproxy "Wi-Fi" "localhost" 7890
+networksetup -setsecurewebproxystate "Wi-Fi" on
+networksetup -setsecurewebproxy "Wi-Fi" "localhost" 7890
+networksetup -setsocksfirewallproxystate "Wi-Fi" on
+networksetup -setsocksfirewallproxy "Wi-Fi" "localhost" 7890
