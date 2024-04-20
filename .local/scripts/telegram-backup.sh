1dd6cc265e2800f7a0df6c9cf3c38fcfaa3cc266
OrkWard <orkward_o@outlook.com>
2024-04-20 12:59:31 +0800

    Sat Apr 20 12:59:31 PM CST 2024

--- .local/scripts/telegram-backup.sh
+++ .local/scripts/telegram-backup.sh
@@ -31,7 +31,7 @@ while [ -n "$1" ]; do
     tmp_json=$(mktemp)
     tdl chat export -c $1 -T id -i $((max_id + 1)) -o "$tmp_json"
     tdl dl -f $tmp_json -d "$backup_dir/$1"
-    jq -s '{id: .[0].id, messages: [.[].messages[]]}' $tmp_json "$backup_dir/$1/export.json" > "$backup_dir/$1/export.json"
+    jq -s '{id: .[0].id, messages: [.[] | .messages[]]}' $tmp_json "$backup_dir/$1/export.json" > "$backup_dir/$1/export.json"
     [ -f tmp_json ] && rm tmp_json
   else
     tdl chat export -c $1 -o "$backup_dir/$1/export.json"
