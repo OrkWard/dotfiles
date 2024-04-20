37a23fd2164bd23f4a7fd07545e9e123aa970a61
OrkWard <orkward_o@outlook.com>
2024-04-20 13:06:45 +0800

    Sat Apr 20 01:06:45 PM CST 2024

--- .local/scripts/telegram-backup.sh
+++ .local/scripts/telegram-backup.sh
@@ -26,16 +26,26 @@ shift "$(($OPTIND - 1))"
 while [ -n "$1" ]; do
   # if directory not exist, create it
   [ -d "$backup_dir/$1" ] || mkdir -p "$backup_dir/$1"
-  if [ -f "$backup_dir/$1/export.json" ]; then
-    max_id=$(cat "$backup_dir/$1/export.json" | jq '.messages[].id' | sort -nr | head -n 1)
+  chat_dir="$backup_dir/$1" # chat backup dir
+  chat_export="$backup_dir/$1/export.json" # chat export.json file
+
+  if [ -f $chat_export ]; then
+    max_id=$(cat $chat_export | jq '.messages[].id' | sort -nr | head -n 1)
     tmp_json=$(mktemp)
+    tmp_json2=$(mktemp)
+
+    # new message
     tdl chat export -c $1 -T id -i $((max_id + 1)) -o "$tmp_json"
     tdl dl -f $tmp_json -d "$backup_dir/$1"
-    jq -s '{id: .[0].id, messages: [.[] | .messages[]]}' $tmp_json "$backup_dir/$1/export.json" > "$backup_dir/$1/export.json"
-    [ -f tmp_json ] && rm tmp_json
+    jq -s '{id: .[0].id, messages: [.[] | .messages[]]}' $tmp_json $chat_export > "$tmp_json2"
+    mv "$tmp_json2" $chat_export
+
+    # rm tmp
+    [ -f $tmp_json ] && rm $tmp_json
+    [ -f $tmp_json2 ] && rm $tmp_json2
   else
-    tdl chat export -c $1 -o "$backup_dir/$1/export.json"
-    tdl dl -f "$backup_dir/$1/export.json" -d "$backup_dir/$1"
+    tdl chat export -c $1 -o $chat_export 
+    tdl dl -f $chat_export -d $chat_dir
   fi
   shift
 done
