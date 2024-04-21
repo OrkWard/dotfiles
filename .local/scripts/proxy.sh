1c35f695586c814e9d3197368612a871352a7781
caizelun <caizelun.1@bytedance.com>
2024-04-21 18:00:01 +0800

    backup: Sun Apr 21 18:00:01 CST 2024

--- .local/scripts/proxy.sh
+++ .local/scripts/proxy.sh
@@ -1,5 +1,3 @@
-echo $1
-
-alias setproxy='export https_proxy=http://127.0.0.1:7890;export http_proxy=http://127.0.0.1:7890;export all_proxy=socks5://127.0.0.1:7890'
-alias unproxy='unset http_proxy https_proxy all_proxy'
-export https_proxy=http://127.0.0.1:7890;export http_proxy=http://127.0.0.1:7890;export all_proxy=socks5://127.0.0.1:7890
+proxy_host=${1:-'127.0.0.1:7890'}
+echo "Set proxy to $proxy_host."
+export https_proxy=http://$proxy_host;export http_proxy=http://$proxy_host;export all_proxy=socks5://$proxy_host
