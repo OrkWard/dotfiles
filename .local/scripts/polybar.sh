c294c0762f231be0e1167b9f3d9c5e788246d090
OrkWard <orkward_o@outlook.com>
2024-07-07 13:30:01 +0800

    backup: Sun Jul  7 01:30:01 PM HKT 2024

--- .local/scripts/polybar.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Terminate already running bar instances
-polybar-msg cmd quit
-
-echo "---" | tee -a /tmp/polybar.log
-polybar 2>&1 | tee -a /tmp/polybar.log & disown
-
-echo "Bars launched..."
