535b8e8d94abd43d96d986ae5f308c8d3ed627cb
caizelun <caizelun.2@bytedance.com>
2024-08-03 16:00:05 +0800

    backup: Sat Aug  3 16:00:05 CST 2024

--- .local/scripts/proxy.sh
+++ .local/scripts/proxy.sh
@@ -1,3 +1,3 @@
-proxy_host=${1:-'localhost:7890'}
+proxy_host=${1:-'localhost:9090'}
 echo "Set proxy to $proxy_host."
 export https_proxy=http://$proxy_host;export http_proxy=http://$proxy_host;export all_proxy=http://$proxy_host
