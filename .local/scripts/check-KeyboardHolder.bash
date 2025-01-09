e43049e152a5b958c0f92b65776eb3726df2dbe6
caizelun <caizelun.2@bytedance.com>
2025-01-09 14:30:07 +0800

    backup: Thu Jan  9 14:30:06 CST 2025

--- .local/scripts/check-KeyboardHolder.bash
+++ .local/scripts/check-KeyboardHolder.bash
@@ -4,3 +4,8 @@ if ! pgrep -x "KeyboardHolder" > /dev/null
 then
     osascript -e 'display notification "KeyboardHolder is not running!" with title "Alert"'
 fi
+
+if ! pgrep -x "Mos" > /dev/null
+then
+    osascript -e 'display notification "Mos is not running!" with title "Alert"'
+fi
