127568fb410280482c1070c3ed3b7959da2c7670
caizelun <caizelun.2@bytedance.com>
2025-01-13 20:00:06 +0800

    backup: Mon Jan 13 20:00:06 CST 2025

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,3 +1,3 @@
-hs.hotkey.bind({"shift", "alt"}, "W", function()
-  hs.notify.new({title="Hammerspoon", informativeText="Hello World"}):send()
-end)
+-- hs.hotkey.bind({"shift", "alt"}, "W", function()
+--   hs.notify.new({title="Hammerspoon", informativeText="Hello World"}):send()
+-- end)
