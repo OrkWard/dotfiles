545daee54aac6aa726305c6928e6a3b2856c098b
caizelun <caizelun.2@bytedance.com>
2025-01-20 11:00:06 +0800

    backup: Mon Jan 20 11:00:05 CST 2025

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,3 +1,20 @@
 -- hs.hotkey.bind({"shift", "alt"}, "W", function()
 --   hs.notify.new({title="Hammerspoon", informativeText="Hello World"}):send()
 -- end)
+
+hs.hotkey.bind({"alt"}, "1", function ()
+  hs.application.launchOrFocus("Alacritty")
+end)
+
+hs.hotkey.bind({"alt"}, "2", function ()
+  hs.application.launchOrFocus("Google Chrome")
+end)
+
+hs.hotkey.bind({"alt"}, "3", function ()
+  hs.application.launchOrFocus("Zed")
+end)
+
+hs.hotkey.bind({"alt"}, "4", function ()
+  hs.application.launchOrFocus("Lark")
+end)
+
