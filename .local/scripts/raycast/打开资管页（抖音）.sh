f92c5bc6d0a02b8e69c4a6e5016c48c4670647fb
caizelun <caizelun.2@bytedance.com>
2025-01-26 15:00:07 +0800

    backup: Sun Jan 26 15:00:07 CST 2025

--- .local/scripts/raycast/打开资管页（抖音）.sh
+++ .local/scripts/raycast/打开资管页（抖音）.sh
@@ -3,10 +3,10 @@
 # Required parameters:
 # @raycast.schemaVersion 1
 # @raycast.title 打开资管页（抖音）
-# @raycast.mode compact
+# @raycast.mode silent
 
 # Optional parameters:
-# @raycast.icon 
+# @raycast.icon https://lf1-fe.ecombdstatic.com/obj/eden-cn/upqphj/homepage/icon.svg
 
 # Documentation:
 # @raycast.author orkward
@@ -15,5 +15,4 @@
 cd ~/Repos/h5
 export PATH=~/.local/bin/:$PATH
 mise exec node@20 -- spm openurl --schema "snssdk1128://webview?url=https%3A%2F%2Flianmeng.snssdk.com%2Fh5%2F_daren%2Faccount%2Fmanage"
-echo "tell application \"System Events\" to key code 53" | osascript
 
