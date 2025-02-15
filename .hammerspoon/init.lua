81bef662a09ab00b6c9c99debcadb87e87402cd6
orkward <orkward_o@outlook.com>
2025-02-15 20:30:38 +0800

    backup: Sat Feb 15 20:30:37 CST 2025

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -5,44 +5,47 @@
 require('hs.ipc')
 
 ---------------- Appliction -----------------
-hs.hotkey.bind({"alt"}, "1", function ()
-  hs.application.launchOrFocus("Alacritty")
+hs.hotkey.bind({ "alt" }, "1", function()
+    hs.application.launchOrFocus("Alacritty")
 end)
 
-hs.hotkey.bind({"alt"}, "2", function ()
-  hs.application.launchOrFocus("Google Chrome")
+hs.hotkey.bind({ "alt" }, "2", function()
+    hs.application.launchOrFocus("Google Chrome")
 end)
 
-hs.hotkey.bind({"alt"}, "3", function ()
-  hs.application.launchOrFocus("Zed")
+hs.hotkey.bind({ "alt" }, "3", function()
+    hs.application.launchOrFocus("Zed")
 end)
 
-hs.hotkey.bind({"alt"}, "4", function ()
-  hs.application.launchOrFocus("Lark")
+hs.hotkey.bind({ "alt" }, "4", function()
+    hs.application.launchOrFocus("Lark")
 end)
 
-hs.hotkey.bind({"alt"}, "0", function ()
-  hs.application.launchOrFocus("Telegram")
+hs.hotkey.bind({ "alt" }, "0", function()
+    hs.application.launchOrFocus("Telegram")
 end)
 
-hs.hotkey.bind({"alt"}, "9", function ()
-  hs.application.launchOrFocus("Proxyman")
+hs.hotkey.bind({ "alt" }, "9", function()
+    hs.application.launchOrFocus("Proxyman")
 end)
 
-hs.hotkey.bind({"alt"}, "8", function ()
-  hs.application.launchOrFocus("Figma")
+hs.hotkey.bind({ "alt" }, "8", function()
+    hs.application.launchOrFocus("Figma")
 end)
 
-hs.hotkey.bind({"alt", "shift"}, "C", function ()
-  hs.application.launchOrFocus("CotEditor")
+hs.hotkey.bind({ "alt", "shift" }, "C", function()
+    hs.application.launchOrFocus("CotEditor")
 end)
 
-hs.hotkey.bind({"alt", "shift"}, "G", function ()
-  hs.application.launchOrFocus("Google Chrome Canary")
+hs.hotkey.bind({ "alt", "shift" }, "G", function()
+    hs.application.launchOrFocus("Google Chrome Canary")
 end)
 
-hs.hotkey.bind({"alt", "shift"}, "R", function ()
-  hs.reload()
+hs.hotkey.bind({ "alt", "shift" }, "R", function()
+    hs.reload()
 end)
 hs.alert.show("Config Reload")
 
+hs.hotkey.bind({ "alt", "shift" }, "D", function()
+    hs.appfinder
+end)
