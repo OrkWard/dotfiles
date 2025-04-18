564d63c97d9c848c22bc1abb6861c660a7203f2e
orkward <orkward_o@outlook.com>
2025-04-18 21:00:01 +0800

    backup: Fri Apr 18 21:00:01 CST 2025

---
+++ .hammerspoon/init.lua
@@ -0,0 +1,41 @@
+require('hs.ipc')
+require('keymaps')
+local inspect = require('lib.inspect')
+
+local inputSourceMap = {
+    ["Alacritty"] = "ABC",
+    ["Zed"] = "ABC",
+    ["MacVim"] = "ABC",
+    ["CotEditor"] = "微信输入法",
+    ["飞书"] = "微信输入法",
+}
+local inputSourceType = {
+  ["ABC"] = "layout",
+  ["Hiragana"] = "method",
+  ["微信输入法"] = "method"
+}
+
+local function startInputSourceSwitch()
+  local appsToWatch = {}
+  for k in pairs(inputSourceMap) do
+    table.insert(appsToWatch, k)
+  end
+
+  hs.window.filter.new(appsToWatch):subscribe(hs.window.filter.windowFocused, function(_, name)
+    local r = true
+    local source = inputSourceMap[name]
+
+    if inputSourceType[source] == "layout" then
+      r = hs.keycodes.setLayout(source)
+    elseif inputSourceType[source] == "method" then
+      r = hs.keycodes.setMethod(source)
+    else
+      hs.alert.show(string.format('sourceName: %s is not layout or method', sourceName))
+    end
+
+    if not r then
+      hs.alert.show(string.format('set %s to %s failure', appName, sourceName))
+    end
+  end)
+end
+startInputSourceSwitch()
