97578b15daa24f324bdf010f23477537c7229abf
caizelun <caizelun.2@bytedance.com>
2025-06-05 17:30:06 +0800

    backup: Thu Jun  5 17:30:06 CST 2025

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -3,40 +3,41 @@ require("keymaps")
 local inspect = require("inspect")
 
 local inputSourceMap = {
-	["Alacritty"] = "ABC",
-	["Zed"] = "ABC",
-	["MacVim"] = "ABC",
-	["CotEditor"] = "微信输入法",
-	["飞书"] = "微信输入法",
-	["Android Studio"] = "ABC",
+  ["Alacritty"] = "ABC",
+  ["Zed"] = "ABC",
+  ["MacVim"] = "ABC",
+  ["CotEditor"] = "微信输入法",
+  ["飞书"] = "微信输入法",
+  ["Android Studio"] = "ABC",
+  ["qutebrowser"] = "ABC",
 }
 local inputSourceType = {
-	["ABC"] = "layout",
-	["Hiragana"] = "method",
-	["微信输入法"] = "method",
+  ["ABC"] = "layout",
+  ["Hiragana"] = "method",
+  ["微信输入法"] = "method",
 }
 
 local function startInputSourceSwitch()
-	local appsToWatch = {}
-	for k in pairs(inputSourceMap) do
-		table.insert(appsToWatch, k)
-	end
+  local appsToWatch = {}
+  for k in pairs(inputSourceMap) do
+    table.insert(appsToWatch, k)
+  end
 
-	hs.window.filter.new(appsToWatch):subscribe(hs.window.filter.windowFocused, function(_, name)
-		local r = true
-		local source = inputSourceMap[name]
+  hs.window.filter.new(appsToWatch):subscribe(hs.window.filter.windowFocused, function(_, name)
+    local r = true
+    local source = inputSourceMap[name]
 
-		if inputSourceType[source] == "layout" then
-			r = hs.keycodes.setLayout(source)
-		elseif inputSourceType[source] == "method" then
-			r = hs.keycodes.setMethod(source)
-		else
-			hs.alert.show(string.format("sourceName: %s is not layout or method", sourceName))
-		end
+    if inputSourceType[source] == "layout" then
+      r = hs.keycodes.setLayout(source)
+    elseif inputSourceType[source] == "method" then
+      r = hs.keycodes.setMethod(source)
+    else
+      hs.alert.show(string.format("sourceName: %s is not layout or method", sourceName))
+    end
 
-		if not r then
-			hs.alert.show(string.format("set %s to %s failure", appName, sourceName))
-		end
-	end)
+    if not r then
+      hs.alert.show(string.format("set %s to %s failure", appName, sourceName))
+    end
+  end)
 end
 startInputSourceSwitch()
