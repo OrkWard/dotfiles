bb74917a8eaed2c90d742da823f0b8e753567a45
caizelun <caizelun.2@bytedance.com>
2025-09-23 22:00:06 +0800

    backup: Tue Sep 23 22:00:06 CST 2025

--- .config/bash/ni.bash
+++ .config/bash/ni.bash
@@ -2,6 +2,11 @@
 
 if type complete &>/dev/null; then
   _nr_completion() {
+    if ni -v 2>&1 | grep -q "no lock file"; then
+      COMPREPLY=()
+      return 0
+    fi
+
     local words
     local cur
     local cword
