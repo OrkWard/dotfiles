73a9891f8b2fb6e596ce105e08d09ff0b11eecc5
orkward <orkward_o@outlook.com>
2025-09-27 22:00:01 +0800

    backup: Sat Sep 27 22:00:01 CST 2025

--- .config/bash/node/corepack.bash
+++ .config/bash/node/corepack.bash
@@ -1,8 +1,2 @@
 export COREPACK_ENABLE_AUTO_PIN=0
 export COREPACK_ENABLE_DOWNLOAD_PROMPT=1
-alias yarn="corepack yarn"
-alias yarnpkg="corepack yarnpkg"
-alias pnpm="corepack pnpm"
-alias pnpx="corepack pnpx"
-alias npm="corepack npm"
-alias npx="corepack npx"
