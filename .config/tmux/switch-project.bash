60d9be47236fc7454e2f5d182838bf2db74b2eff
caizelun <caizelun.2@bytedance.com>
2025-10-20 12:00:07 +0800

    backup: Mon Oct 20 12:00:07 CST 2025

--- .config/tmux/switch-project.bash
+++ .config/tmux/switch-project.bash
@@ -7,8 +7,14 @@ if [ -z "$projects" ]; then
   exit 1
 fi
 
+if [[ "$1" != "-n" ]]; then
+  prompt="Switch to project: "
+else
+  prompt="Open project: "
+fi
+
 selected=$(echo "$projects" | fzf-tmux -p 80%,60% \
-  --prompt="Switch to project: " \
+  --prompt="$prompt" \
   --border \
   --preview 'cat ~/.config/tmuxinator/{}.yml 2>/dev/null || echo "No config file"' \
   --preview-window=right:50%)
