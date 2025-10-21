e343eec2246fe5db5588c09f03a757044ccdb050
caizelun <caizelun.2@bytedance.com>
2025-10-21 16:00:07 +0800

    backup: Tue Oct 21 16:00:07 CST 2025

--- .config/fish/conf.d/02-alias.fish
+++ .config/fish/conf.d/02-alias.fish
@@ -1,38 +1,49 @@
 # handy
-alias rm='rm -r'
-alias mkdir='mkdir -p'
+alias rm 'rm -r'
+alias mkdir 'mkdir -p'
 
 # cd
-alias ..='cd ..'
-alias ...='cd ../..'
-alias ....='cd ../../..'
+alias .. 'cd ..'
+alias ... 'cd ../..'
+alias .... 'cd ../../..'
 
 # vim
-alias vn="nvim ~/.config/nvim/init.lua"
-alias vf="tmuxinator fish"
-alias vv="tmuxinator vim-plug"
-alias vg="tmuxinator ghostty"
-alias vt="vim ~/.config/tmux/tmux.conf"
+alias vn "nvim ~/.config/nvim/init.lua"
+alias vf "tmuxinator fish"
+alias vv "tmuxinator vim-plug"
+alias vg "tmuxinator ghostty"
+alias vt "vim ~/.config/tmux/tmux.conf"
 
 
 # yadm
-alias yac='yadm add -u && yadm commit -m "$(date)"'
-alias ys='yadm status'
-alias yd='yadm diff'
-alias yl='yadm pull'
-alias yp='yadm push'
+alias yac 'yadm add -u && yadm commit -m "$(date)"'
+alias ys 'yadm status'
+alias yd 'yadm diff'
+alias yl 'yadm pull'
+alias yp 'yadm push'
 
 # ls
-alias l='ls --color'
-alias ls='ls --color'
-alias ll='ls --color -alh'
-alias la='ls --color -a'
-alias recent='ls --color -tlh | head'
+alias l 'ls --color'
+alias ls 'ls --color'
+alias ll 'ls --color -alh'
+alias la 'ls --color -a'
+alias recent 'ls --color -tlh | head'
 
-alias bat="bat --theme=GitHub"
-alias serve="caddy file-server . --listen localhost:8080"
-alias tree="lsd --tree --depth 2"
+alias bat "bat --theme=GitHub"
+alias serve "caddy file-server . --listen localhost:8080"
+alias tree "lsd --tree --depth 2"
 
 # zed
-alias z='zed'
+alias z 'zed'
 
+# git
+alias ga 'git add -A'
+alias gc 'git commit'
+alias gac 'git add . && git commit -m "$(date)"'
+alias gs 'git status'
+alias gd 'git diff'
+alias gds 'git diff --staged'
+alias gl 'git pull'
+alias gp 'git push'
+alias gca 'git commit --amend'
+alias gpf 'git push --force-with-lease'
--- .config/fish/conf.d/11-tool.fish
+++ .config/fish/conf.d/11-tool.fish
@@ -4,3 +4,17 @@ atuin init fish | sed 's/-k up/up/' | source
 # direnv
 direnv hook fish | source
 
+# asdf
+set -gx ASDF_CONFIG_FILE $HOME/.config/asdf/asdfrc
+set -gx ASDF_DATA_DIR $HOME/.local/state/asdf
+fish_add_path $ASDF_DATA_DIR/shims
+
+# autojump
+if [ $(uname -s) = "Darwin" ]
+  [ -f /opt/homebrew/share/autojump/autojump.fish ] && . /opt/homebrew/share/autojump/autojump.fish
+end
+
+# corepack
+set -gx COREPACK_ENABLE_AUTO_PIN 0
+set -gx COREPACK_ENABLE_DOWNLOAD_PROMPT 1
+
