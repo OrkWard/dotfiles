15a8259947e60e79433b88bb13d9c20636dd1024
caizelun <caizelun.2@bytedance.com>
2025-10-21 18:00:05 +0800

    backup: Tue Oct 21 18:00:05 CST 2025

---
+++ .config/tmux/scripts/switch-project.fish
@@ -0,0 +1,31 @@
+#!/usr/bin/env fish
+set projects $(tmuxinator list -n 2>/dev/null | tail -n +2)
+
+if test -z "$projects"
+  tmux display-message "No tmuxinator projects found"
+  exit 1
+end
+
+if test "$argv[1]" != "-n"
+  set prompt "Switch to project: "
+else
+  set prompt "Open project: "
+end
+
+set selected $(string join \n $projects | fzf-tmux -p 80%,60% \
+  --prompt=$prompt \
+  --border \
+  --preview 'cat ~/.config/tmuxinator/{}.yml 2>/dev/null || echo "No config file"' \
+  --preview-window=right:50%)
+
+if test -n "$selected"
+  set current_session $(tmux display-message -p '#S')
+
+  tmuxinator start $selected --no-attach &>/dev/null
+
+  tmux switch-client -t $selected 2>/dev/null
+
+  if test $current_session != $selected -a $argv[1] != -n
+    tmux kill-session -t $current_session 2>/dev/null
+  end
+end
