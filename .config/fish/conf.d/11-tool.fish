edf6c2e49af8afcbeb56bcb1bc088933ff6f69ba
caizelun <caizelun.2@bytedance.com>
2025-11-07 12:00:06 +0800

    backup: Fri Nov  7 12:00:06 CST 2025

--- .config/fish/conf.d/11-tool.fish
+++ .config/fish/conf.d/11-tool.fish
@@ -32,13 +32,13 @@ fish_add_path $PNPM_HOME
 set -xg _ZL_CMD j
 set -xg _ZL_DATA_DIR $HOME/.local/share/zlua/
 set -xg _ZL_DATA $HOME/.local/share/zlua/zlua.txt
-if not type -q lua; or not type -q czmod
-  echo "z.lua deps not complete"
+if not type -q lua
+  echo "lua not found"
+else if not type -q czmod
+  echo "czmod not found"
 else
   z.lua --init fish | source
   function _zlua_precmd --on-event fish_prompt
-      czmod --add "$PWD" 2> /dev/null &
+    czmod --add "$PWD" 2> /dev/null &
   end
-  abbr jb "j -b"
 end
-
