52cbaaa2e1c0c7bc4244f9f42ecd008021fd5038
OrkWard <orkward_o@outlook.com>
2025-12-19 00:00:08 +0800

    backup: Fri Dec 19 00:00:08 CST 2025

--- .config/fish/conf.d/10-env.fish
+++ .config/fish/conf.d/10-env.fish
@@ -1,4 +1,4 @@
-set -gx EDITOR vim
+set -gx EDITOR hx
 set -gx LC_ALL en_US.UTF-8
 set -gx LC_CTYPE en_US.UTF-8 # for mosh on MacOS
 set -gx LANG en_US.UTF-8
@@ -10,7 +10,6 @@ set fish_greeting
 # gcc
 set -gx GCC_COLORS 'error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
 
-if [ "$TERM_PROGRAM" = 'zed' ]
+if [ "$TERM_PROGRAM" = zed ]
     set -gx EDITOR "zed -w"
 end
-
