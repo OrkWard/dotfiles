0ef9ac70f42edd99dc8ecc7b39577939e298a475
caizelun <caizelun.2@bytedance.com>
2025-12-19 18:00:07 +0800

    backup: Fri Dec 19 18:00:07 CST 2025

--- .config/fish/conf.d/00-path.fish
+++ .config/fish/conf.d/00-path.fish
@@ -1,12 +1,13 @@
+# local bin
 fish_add_path $HOME/.local/bin
 
 # homebrew
-if test $(uname -s) = "Darwin"
-  eval $(/opt/homebrew/bin/brew shellenv)
-  set -gx HOMEBREW_NO_ENV_HINTS 1
+if test $(uname -s) = Darwin
+    eval $(/opt/homebrew/bin/brew shellenv)
+    set -gx HOMEBREW_NO_ENV_HINTS 1
 end
 
 # rustup
-if test $(uname -s) = "Darwin"
-  fish_add_path --move $(brew --prefix rustup)/bin
+if test $(uname -s) = Darwin
+    fish_add_path --move $(brew --prefix rustup)/bin
 end
