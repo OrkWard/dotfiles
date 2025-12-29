c7478a8aed05853b82db1527d9f8f4b2ca828fd7
caizelun <caizelun.2@bytedance.com>
2025-12-29 18:00:06 +0800

    backup: Mon Dec 29 18:00:06 CST 2025

--- .config/fish/functions/copy.fish
+++ .config/fish/functions/copy.fish
@@ -1,4 +1,5 @@
+# pipe stdin content to system clipboard
 function copy
-  read -l input
-  printf "\033]52;c;%s\007" (echo -n $input | base64 -w0)
+    read -l input
+    printf "\033]52;c;%s\007" (echo -n $input | base64 -w0)
 end
--- .config/fish/functions/copyfile.fish
+++ .config/fish/functions/copyfile.fish
@@ -1,14 +1,16 @@
+# accpet a file, write a fish command to system clipboard
+# this command ask user output file name and write it
 function copyfile
-  if test -z "$argv[1]"
-    return
-  end
+    if test -z "$argv[1]"
+        return
+    end
 
-  set -l input[1] 'begin;'
-  set -l input[2] 'read -P "filename> " -l file;'
-  set -l input[3] 'echo "'
-  set -l input[4] (cat $argv[1] | base64 -w0)
-  set -l input[5] '" | base64 -d -o $file;'
-  set -l input[6] 'end;'
+    set -l input[1] 'begin;'
+    set -l input[2] 'read -P "filename> " -l file;'
+    set -l input[3] 'echo "'
+    set -l input[4] (cat $argv[1] | base64 -w0)
+    set -l input[5] '" | base64 -d -o $file;'
+    set -l input[6] 'end;'
 
-  printf "\033]52;c;%s\007" (string join " " $input | base64 -w0)
+    printf "\033]52;c;%s\007" (string join " " $input | base64 -w0)
 end
