8a9dc69c9d806592da40526c0b1219dd3ea0aa0b
OrkWard <71199274+OrkWard@users.noreply.github.com>
2026-02-26 17:39:01 +0800

    Update 00-path.fish

--- .config/fish/conf.d/00-path.fish
+++ .config/fish/conf.d/00-path.fish
@@ -8,8 +8,3 @@ if test $(uname -s) = Darwin
     eval $(/opt/homebrew/bin/brew shellenv)
     set -gx HOMEBREW_NO_ENV_HINTS 1
 end
-
-# rustup
-if test $(uname -s) = Darwin
-    fish_add_path --move $(brew --prefix rustup)/bin
-end
